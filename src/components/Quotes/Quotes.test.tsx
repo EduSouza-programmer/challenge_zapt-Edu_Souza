1fecc50b456f8688ed24cf465dad5ccad8ac7595
Eduardo Souza <dado.eletrons@gmail.com>
2021-07-17 20:54:53 -0300

    :white_check_mark: test: Created test the component

--- src/components/Quotes/Quotes.test.tsx
+++ src/components/Quotes/Quotes.test.tsx
@@ -4,13 +4,30 @@ import { renderWithTheme } from "@/utils/tests/helpers";
 import Quotes from ".";
 
 describe("<Quotes />", () => {
-  it("should render the heading", () => {
-    const { container } = renderWithTheme(<Quotes />);
+  it("should render a children correctly", () => {
+    renderWithTheme(
+      <Quotes>
+        <h1>children</h1>
+      </Quotes>
+    );
 
     expect(
-      screen.getByRole("heading", { name: /Quotes/i })
+      screen.getByRole("heading", { name: /children/i })
     ).toBeInTheDocument();
+  });
 
-    expect(container.firstChild).toMatchSnapshot();
+  it("should render two quotes correctly", () => {
+    renderWithTheme(
+      <Quotes>
+        <h1>children</h1>
+      </Quotes>
+    );
+
+    expect(
+      screen.getByRole("img", { name: /quotes top left/i })
+    ).toBeInTheDocument();
+    expect(
+      screen.getByRole("img", { name: /quotes bottom right/i })
+    ).toBeInTheDocument();
   });
 });
