c487643103becf7421058d19301fa2b40d395cd9
Eduardo Souza <dado.eletrons@gmail.com>
2021-07-18 06:15:21 -0300

    :clown_face: clown-face: Created mock data <Carousel/>

---
+++ src/components/Carousel/Carousel.mock.ts
@@ -0,0 +1,44 @@
+import { CardSliderProps } from "@/components/CardSlider";
+
+export default [
+  {
+    user: "Patricia Godoy",
+    city: "São Paulo",
+    avatar: "img/patricia_godoy.png",
+    uf: "SP",
+    feedback:
+      "”Não consigo mais viver sem os produtos do Sítio, a diferença do que eu comprava antes e agora é abissal”",
+    backgroundImage:
+      "https://i.pstimaj.com/img/78/740x0/5fd9f0cb66a97cbb3100a5b2.jpg",
+  },
+  {
+    user: "Patricia Godoy",
+    city: "São Paulo",
+    avatar: "img/patricia_godoy.png",
+    uf: "SP",
+    feedback:
+      "”Não consigo mais viver sem os produtos do Sítio, a diferença do que eu comprava antes e agora é abissal”",
+    backgroundImage:
+      "https://img.srgcdn.com/e//Z0pRWWpuWUpGZ3g1R2FKb3pWR3AuanBn.jpg",
+  },
+  {
+    user: "Patricia Godoy",
+    city: "São Paulo",
+    avatar: "img/patricia_godoy.png",
+    uf: "SP",
+    feedback:
+      "”Não consigo mais viver sem os produtos do Sítio, a diferença do que eu comprava antes e agora é abissal”",
+    backgroundImage:
+      "https://images.unsplash.com/photo-1606787620819-8bdf0c44c293?ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80",
+  },
+  {
+    user: "Patricia Godoy",
+    city: "São Paulo",
+    avatar: "img/patricia_godoy.png",
+    uf: "SP",
+    feedback:
+      "”Não consigo mais viver sem os produtos do Sítio, a diferença do que eu comprava antes e agora é abissal”",
+    backgroundImage:
+      "https://images.unsplash.com/photo-1556910103-1c02745aae4d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80",
+  },
+] as CardSliderProps[];
