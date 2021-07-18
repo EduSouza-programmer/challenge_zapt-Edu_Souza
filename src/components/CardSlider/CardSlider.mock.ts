c36e4a0af611247de56c2065b8cf8886bf8b9efd
Eduardo Souza <dado.eletrons@gmail.com>
2021-07-18 04:42:15 -0300

    :clown_face: clown-face: Created mock <CardSli…

---
+++ src/components/CardSlider/CardSlider.mock.ts
@@ -0,0 +1,12 @@
+import { CardSliderProps } from ".";
+
+export default {
+  user: "Patricia Godoy",
+  city: "São Paulo",
+  avatar: "img/patricia_godoy.png",
+  uf: "SP",
+  feedback:
+    "”Não consigo mais viver sem os produtos do Sítio, a diferença do que eu comprava antes e agora é abissal”",
+  backgroundImage:
+    "https://i.pstimaj.com/img/78/740x0/5fd9f0cb66a97cbb3100a5b2.jpg",
+} as CardSliderProps;
