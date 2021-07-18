7c4df7a9accfff70561392cc61569172b7b1b7ff
Eduardo Souza <dado.eletrons@gmail.com>
2021-07-18 03:02:43 -0300

    :white_check_mark: test: Created test for compon…
    Starting the component with TDD.
    
    Closes #5

---
+++ src/components/Welcome/Welcome.test.tsx
@@ -0,0 +1,36 @@
+import { screen } from "@testing-library/react";
+import { renderWithTheme } from "@/utils/tests/helpers";
+
+import welcomeMock from "./Welcome.mock";
+import Welcome from ".";
+
+const props = welcomeMock;
+
+describe("<Welcome />", () => {
+  it("should render the username and message correctly when passed", () => {
+    renderWithTheme(<Welcome {...props} />);
+
+    expect(
+      screen.getByRole("heading", { name: /bem vindo marcio/i })
+    ).toBeInTheDocument();
+    expect(screen.getByText(props.message)).toBeInTheDocument();
+  });
+
+  it("should render a image by default", () => {
+    renderWithTheme(<Welcome {...props} />);
+
+    expect(screen.getByRole("img", { name: /happy people/i })).toHaveAttribute(
+      "src",
+      "img/Mask_Group.png"
+    );
+  });
+
+  it("should render a image when passed", () => {
+    renderWithTheme(<Welcome {...props} imageBanner="my_image" />);
+
+    expect(screen.getByRole("img", { name: /happy people/i })).toHaveAttribute(
+      "src",
+      "my_image"
+    );
+  });
+});
